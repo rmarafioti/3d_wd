38915e7ebb017014068928f01653ee570cc81ea3
Rich Marafioti <richmarafioti@gmail.com>
2026-09-11 08:09:59 -0500

    test: feat change in GH

--- src/app/page.js
+++ src/app/page.js
@@ -14,10 +14,7 @@ export default function Home() {
           priority
         />
         <div className={styles.intro}>
-          <h1>
-            To get started, edit the{" "}
-            <code className={styles.code}>page.js</code> file.
-          </h1>
+          <h1>Welcome to 3D WD!</h1>
           <p>
             Looking for a starting point or more instructions? Head over to{" "}
             <a
